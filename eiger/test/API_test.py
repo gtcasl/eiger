df239adcd107bb58dea0545d60e117ce5eaebc68
Eric Anger <eanger@users.noreply.github.com>
2013-06-10 15:32:50 -0700

    starting the tests

--- eiger/test/API_test.py
+++ eiger/test/API_test.py
@@ -1,11 +1,23 @@
 import unittest
-class TestAPI(unittest.TestCase):
-	def test_one(self):
-		self.assertEqual(1,2)
+from API import DataCollection
 
-	""" Now we should start adding regression tests to ensure that this bad boi
-	functions correctly. """
+class TestDataCollection(unittest.TestCase):
+	"""
+	Each test method needs "test" in the beginning of the name.
 
+		def test_one(self):
+			self.assertEqual(1,2)
+
+	"""
+	def setUp(self):
+		self.dc = DataCollection()
+
+	def testEmpty(self):
+		self.assertTrue(self.dc.machines.empty())
+		self.assertTrue(self.dc.applications.empty())
+		self.assertTrue(self.dc.trials.empty())
+		self.assertTrue(self.dc.datasets.empty())
+		self.assertTrue(self.dc.metrics.empty())
 
 
 
