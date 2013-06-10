0f32b206ba56cc1b0d0c94a38ec4e43a81e38b67
Eric Anger <eanger@gmail.com>
2013-06-10 17:09:46 -0400

    stubbed testing files

---
+++ eiger/test/API_test.py
@@ -0,0 +1,15 @@
+import unittest
+class TestAPI(unittest.TestCase):
+	def test_one(self):
+		self.assertEqual(1,2)
+
+	""" Now we should start adding regression tests to ensure that this bad boi
+	functions correctly. """
+
+
+
+
+
+if __name__ == "__main__":
+	unittest.main()
+
