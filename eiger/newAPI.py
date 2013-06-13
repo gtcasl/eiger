77371b16a10216d44cae754584b16feb139711ab
Eric Anger <eanger@gmail.com>
2013-06-12 20:08:36 -0400

    use newAPI.DataCollection to load in data from db.

--- eiger/newAPI.py
+++ eiger/newAPI.py
@@ -1,87 +1,121 @@
+import numpy as np
+
 class DataCollection:
 
 	def __init__(self, name, db):
 		""" load a data collection from db """
-		self.profile = np.ndarray((0,0))
+		cursor = db.cursor()
 
-		cursor.execute("""SELECT name,description FROM applications \
-				JOIN trials ON trials.applicatioinID = applications.ID \
-				WHERE trials.dataCollectionID = %s""" (dcid,))
-		self.apps = {app: np.ndarray(0,) for app in cursor.fetchall()}
-		
-		cursor.execute("""SELECT name,description FROM machines  \
-				JOIN trials ON trials.machineID  = machines.ID \
-				WHERE trials.dataCollectionID = %s""" (dcid,))
-		self.machines = {machine: np.ndarray(0,) \
-				for machine in cursor.fetchall()}
+		cursor.execute("""SELECT ID FROM datacollections \
+				WHERE name="%s" """ % (name,))
+		self._my_id = cursor.fetchone()[0]
 
-		cursor.execute("""SELECT name,description FROM datasets  \
-				JOIN trials ON trials.datasetID  = datasets.ID \
-				WHERE trials.dataCollectionID = %s""" (dcid,))
-		self.datasets = {dataset: np.ndarray(0,) \
-				for dataset in cursor.fetchall()}
-
-		# gets all metrics, not just ones in this data collection.
-		# TODO: eliminate unused metrics
-		cursor.execute("""SELECT name,description,type FROM metrics""")
-		self.metrics = {metric: np.ndarray(0,) \
-				for metric in cursor.fetchall()}
-		
 		# create consistent mapping of trialID to profile index
 		cursor.execute("""SELECT ID from trials where \
-				dataCollectionID = %s""" % (dcid,))
+				dataCollectionID = %s""" % (self._my_id,))
 		self._trial_id_map = {trialid[0]: index for index,trialid \
-				in enumerate(cursor.fetchall()}
+				in enumerate(cursor.fetchall())}
+
+		self.apps = self._loadObject(cursor, "application")
+		self.machines = self._loadObject(cursor, "machine")
+		self.datasets = self._loadObject(cursor, "dataset")
 
 		# create consistent mapping of metricID to profile index
-		cursor.execute("""SELECT ndm.metricID \
-				FROM nondeterministic_metrics as ndm \
-				JOIN executions as ex \
-				ON ndm.executionID = ex.ID \
-				JOIN trials as tr \
-				ON ex.trialID = tr.ID \
-				where tr.dataCollectionID = %s""" % (dcid,))
-		self._nondet_metric_id_map = {metid[0]: index for index,metid \
-				in enumerate(cursor.fetchall()}
+		cursor.execute(""" \
+				SELECT DISTINCT mets.name,mets.description,mets.type \
+					FROM nondeterministic_metrics as ndm \
+					JOIN metrics as mets \
+					ON mets.ID = ndm.metricId \
+					JOIN executions as ex \
+					ON ndm.executionID = ex.ID \
+					JOIN trials as tr \
+					ON ex.trialID = tr.ID \
+					WHERE tr.dataCollectionID = %s \
+				UNION \
+				SELECT DISTINCT mets.name,mets.description,mets.type \
+					FROM deterministic_metrics as dm \
+					JOIN metrics as mets \
+					ON mets.ID = dm.metricID \
+					JOIN datasets as ds \
+					ON dm.datasetID = ds.ID \
+					JOIN trials as tr \
+					ON ds.ID = tr.datasetID \
+					WHERE tr.dataCollectionID = %s \
+				UNION \
+				SELECT DISTINCT mets.name,mets.description,mets.type \
+					FROM machine_metrics as mm \
+					JOIN metrics as mets \
+					ON mets.ID = mm.metricID \
+					JOIN machines as mach \
+					ON mm.machineID = mach.ID \
+					JOIN trials as tr \
+					ON mach.ID = tr.machineID \
+					WHERE tr.dataCollectionID = %s""" \
+					% (self._my_id, self._my_id, self._my_id))
+		self.metrics = ([], {})
+		for (idx, (name, desc, mtype)) in enumerate(cursor.fetchall()):
+			if (name,desc,mtype) not in self.metrics[0]:
+				self.metrics[0].append((name,desc,mtype))
+			self.metrics[1][name] = idx
 		
 		# get all data now
-		cursor.execute("""SELECT ds.name,mets.name,dm.metric \
-				FROM deterministic_metrics as dm \
-				JOIN datasets as ds \
-				ON dm.datasetID = ds.ID \
-				JOIN trials as t \
-				ON t.datasetID = ds.ID \
-				where t.dataCollectionID = %s""" % (dcid,))
-		self._det_metrics = {dset: {met: val} for (dset,met,val) \
-				in cursor.fetchall()}
-		
-		cursor.execute("""SELECT ma.name,mets.name,mm.metric \
-				FROM machine_metrics as mm \
-				JOIN machines as ma \
-				ON mm.machineID = ma.ID \
-				JOIN trials as t \
-				ON t.machineID = ma.ID \
-				where t.dataCollectionID = %s""" % (dcid,))
-		self._machine_metrics = {machine: {met: val} for (machine,met,val) \
-				in cursor.fetchall()}
-	
 		n_trials = len(self._trial_id_map)
-		n_nondetmets = len(self._nondet_metric_id_map)
-		self._nondet_metrics = np.ndarray((n_trials,n_nondetmets))
-		cursor.execute("""SELECT tr.ID,ndm.metricID,ndm.metric \
-				FROM nondeterministic_metrics as ndm \
-				JOIN executions as ex \
-				ON ndm.executionID = ex.ID \
-				JOIN trials as tr \
-				ON ex.trialID = tr.ID \
-				where tr.dataCollectionID = %s""" % (dcid,))
-		for (trial,metric,value) in cursor.fetchall():
-			self._nondet_metrics[ \
-					self._trial_id_map[trial], \
-					self._nondet_metric_id_map[metric]] = value
+		n_mets = len(self.metrics[1])
+		self.profile = np.ndarray((n_trials,n_mets))
+		cursor.execute(""" \
+				SELECT t.ID,mets.name,dm.metric \
+					FROM deterministic_metrics as dm \
+					JOIN datasets as ds \
+					ON dm.datasetID = ds.ID \
+					JOIN metrics as mets \
+					ON dm.metricID = mets.ID \
+					JOIN trials as t \
+					ON t.datasetID = ds.ID \
+					WHERE t.dataCollectionID = %s \
+				UNION \
+				SELECT tr.ID,mets.name,ndm.metric \
+					FROM nondeterministic_metrics as ndm \
+					JOIN metrics as mets \
+					ON ndm.metricID = mets.ID \
+					JOIN executions as ex \
+					ON ndm.executionID = ex.ID \
+					JOIN trials as tr \
+					ON ex.trialID = tr.ID \
+					WHERE tr.dataCollectionID = %s \
+				UNION \
+				SELECT t.ID,mets.name,mm.metric \
+					FROM machine_metrics as mm \
+					JOIN machines as mach \
+					ON mm.machineID = mach.ID \
+					JOIN metrics as mets \
+					ON mm.metricID = mets.ID \
+					JOIN trials as t \
+					ON t.machineID = mach.ID \
+					WHERE t.dataCollectionID = %s""" % 
+					(self._my_id, self._my_id, self._my_id))
+		for (trial, name, value) in cursor.fetchall():
+			self.profile[self._trial_id_map[trial], self.metrics[1][name]] \
+						= value
 
-	@property
-	def profile(self):
-		""" expand out compressed form of profile into a real matrix """
-		pass
+	def _loadObject(self, cursor, identifier):
+		command = """SELECT trials.id,tbl.name,tbl.description \
+				FROM %ss as tbl \
+				JOIN trials ON trials.%sID = tbl.ID \
+				WHERE trials.dataCollectionID = %s""" % \
+				(identifier, identifier, self._my_id,)
+		cursor.execute(command)
+		destination = ([], {})
+		for (trial,name,desc) in cursor.fetchall():
+			if (name,desc) not in destination[0]:
+				destination[0].append((name,desc))
+			destination[1].setdefault(name, [])
+			destination[1][name].append(self._trial_id_map[trial])
+		return destination
+
+if __name__ == "__main__":
+	import MySQLdb as mdb
+	
+	db = mdb.connect(host='localhost',user='root',passwd='root',db='minimd')
+	d = DataCollection('Nbuild', db)
+	pass
 
