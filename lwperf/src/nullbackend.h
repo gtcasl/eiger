6e3af9858bb7d28fa5619ab9a10c1d5cb97eec52
Eric Anger <eanger@users.noreply.github.com>
2013-11-18 16:21:51 -0500

    give null backend correct interface

--- lwperf/src/nullbackend.h
+++ lwperf/src/nullbackend.h
@@ -4,8 +4,8 @@
 
 class NullBackend{
   public:
-    void writeheaders(const std::map<std::string, enum datakind>& headers) {}
-    void nextrow(const std::map<std::string, enum datakind>& headers,
+    void writeheaders(const std::vector<std::pair<std::string, enum datakind> >& headers) {}
+    void nextrow(const std::vector<std::pair<std::string, enum datakind> >& headers,
                  const std::vector<double>& row) {}
 
     void addCol(const std::string& label, const enum datakind k) {}
