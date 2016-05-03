abe0e7b0c79de83228fa5c2014757187cbb056e9
Eric Anger <eanger@gmail.com>
2016-05-03 15:55:14 -0400

    update setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='Eiger',
 	  version='3.0',
 	  description='Eiger Performance Modeling',
+          url='https://github.com/gtcasl/eiger',
 	  author='Eric Anger',
 	  author_email='eanger@gatech.edu',
 	  packages=['eiger'],
-	  scripts=['Eiger.py']
+	  scripts=['Eiger.py'],
+          install_requires=[
+              'sklearn',
+              'scipy',
+              'matplotlib',
+              'tabulate',
+          ],
+          zip_safe=False
 	 )
 
